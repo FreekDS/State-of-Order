c9a59be334203505b9036fa2c18197cbe9bb6bd1
Freek <freek.de.sagher21@gmail.com>
2025-02-17 17:31:40 +0100

    nen teevee

---
+++ decorations/scripts/televies.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func play():
+	animation_player.play("start")

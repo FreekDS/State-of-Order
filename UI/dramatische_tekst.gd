4b591eb5c9e0aabf57dbf45951a90596207cd9ca
thibautvangoethem <thibautvangoethem2@gmail.com>
2025-02-17 22:21:03 +0100

    deftig met animaties

--- UI/dramatische_tekst.gd
+++ UI/dramatische_tekst.gd
@@ -3,36 +3,20 @@ extends MarginContainer
 @onready var TekstEen = $"CenterContainer/HBoxContainer/1"
 @onready var TekstTwee = $"CenterContainer/HBoxContainer/2"
 @onready var TekstDrie = $"CenterContainer/HBoxContainer/3"
-@onready var timer = $Timer
-
-var tekstCounter = 0
+@onready var anim = $AnimationPlayer
 func dramatiek() -> void:
-	if tekstCounter != 0:
+	if anim.active:
 		print("ellaba ik was nog dramatisch bezig")
 	# Visible changes the location/reorders, modulate is the unnoficial workaround to have the tekst keep its locationø
-	
-	TekstEen.modulate.a = 0
-	TekstTwee.modulate.a = 0
-	TekstDrie.modulate.a = 0
+	anim.play("appear")
 	visible = true
-	tekstCounter = 1
-	timer.start()
-
+	
 func HeyIkWilNieuweTekst(een: String, twee: String, drie: String) -> void:
 	TekstEen.text = een
 	TekstTwee.text = twee
 	TekstDrie.text = drie
 
 
-func _on_timer_timeout() -> void:
-	if tekstCounter == 1:
-		TekstEen.modulate.a = 255
-	elif tekstCounter == 2:
-		TekstTwee.modulate.a = 255
-	elif tekstCounter == 3:	
-		TekstDrie.modulate.a = 255
-	else:
-		visible = false
-		tekstCounter = 0
-	tekstCounter += 1
-	timer.start()
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "appear":
+		anim.play("fade")

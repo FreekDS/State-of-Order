f43e16ac54e1ac39bf569f6e8950af4fda7613fd
Freek <freek.de.sagher21@gmail.com>
2025-02-21 21:15:12 +0100

    gebakken peren

--- entities/scripts/states/leave.gd
+++ entities/scripts/states/leave.gd
@@ -12,10 +12,15 @@ func enter():
 
 
 func tick(_delta):
+	if is_queued_for_deletion():
+		return
 	character.pickable = false
 	var target := navAgent.get_next_path_position()
 	var dir = (target - character.global_position).normalized()
 	character.velocity = dir * speed
+	
+	if character.global_position.distance_to(TARGET) < 10:
+		queue_free()
 
 
 func exit():
